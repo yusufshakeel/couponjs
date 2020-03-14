69199187fe71da0e3b9be7bbf2356202158788b5
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-03-14 14:10:53 +0530

    default option updated

--- app/option.js
+++ app/option.js
@@ -1,8 +1,10 @@
-const {DEFAULT_LENGTH, DEFAULT_PREFIX, DEFAULT_SUFFIX, ALPHABET_UPPERCASE} = require('./constants.js');
+const {DEFAULT_LENGTH, DEFAULT_PREFIX, DEFAULT_SUFFIX, ALPHABET_UPPERCASE, CHARSET_ALPHA} = require('./constants.js');
 
 module.exports = {
   length: DEFAULT_LENGTH,
   prefix: DEFAULT_PREFIX,
   suffix: DEFAULT_SUFFIX,
-  characters: ALPHABET_UPPERCASE
+  characterSet: {
+    builtIn: [CHARSET_ALPHA]
+  }
 };
