57a295d4b8dea0422f7748c3b8cbe0baf4b65f84
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-03-14 14:10:22 +0530

    character set builder + test

---
+++ app/character-set-builder.js
@@ -0,0 +1,41 @@
+const { CHARSET_ALPHA, CHARSET_ALPHA_LOWER, CHARSET_DIGIT, ALPHABET_UPPERCASE, ALPHABET_LOWERCASE, DIGIT} = require('./constants.js');
+
+/**
+ * This will return the characters based on the character set name.
+ * @param {string} charSet This is the name of the character set.
+ * @returns {string} String of characters.
+ */
+function characters (charSet) {
+  switch(charSet) {
+    case CHARSET_ALPHA: return ALPHABET_UPPERCASE;
+    case CHARSET_ALPHA_LOWER: return ALPHABET_LOWERCASE;
+    case CHARSET_DIGIT: return DIGIT;
+    default: throw new Error(`Invalid builtIn characterSet specified. Allowed values ["${CHARSET_ALPHA}", "${CHARSET_ALPHA_LOWER}", "${CHARSET_DIGIT}"]`);
+  }
+}
+
+/**
+ * This will generate a string of unique characters based on the options provided.
+ * @param {object} characterSetOptions The options to build the character set.
+ * @returns {string} The set of characters based on the options provided.
+ */
+function characterSetBuilder(characterSetOptions) {
+
+  const { builtIn = [], custom = [] } = characterSetOptions;
+
+  const builtInCharacters = builtIn.reduce((chars, charSet) => {
+    return `${chars}${characters(charSet)}`
+  }, '');
+
+  const customCharacters = custom.reduce((chars, charSet) => {
+    return `${chars}${charSet}`
+  }, '');
+
+  const uniqueCharacters = `${builtInCharacters}${customCharacters}`.split('').reduce((characters, character) => {
+    return characters + (characters.indexOf(character) === -1 ? character : '');
+  }, '');
+
+  return `${uniqueCharacters}`;
+}
+
+module.exports = characterSetBuilder;
