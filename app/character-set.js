69e82618908ef5d64fb2ae36097247e765932547
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-03-15 11:32:13 +0530

    lint fixes

--- app/character-set.js
+++ app/character-set.js
@@ -22,35 +22,35 @@ const {
  * @returns {string} String of characters.
  */
 function characterSet(charSetName) {
+  const validCharSets = [
+    CHARSET_ALPHA,
+    CHARSET_ALPHA_LOWER,
+    CHARSET_DIGIT,
+    CHARSET_ALNUM,
+    CHARSET_BINARY,
+    CHARSET_OCTAL,
+    CHARSET_HEX,
+    CHARSET_HEX_LOWER
+  ];
   switch (charSetName) {
-    case CHARSET_ALPHA:
-      return ALPHABET_UPPERCASE;
-    case CHARSET_ALPHA_LOWER:
-      return ALPHABET_LOWERCASE;
-    case CHARSET_DIGIT:
-      return DIGIT;
-    case CHARSET_ALNUM:
-      return `${ALPHABET_UPPERCASE}${ALPHABET_LOWERCASE}${DIGIT}`;
-    case CHARSET_BINARY:
-      return `${BINARY}`;
-    case CHARSET_OCTAL:
-      return `${OCTAL}`;
-    case CHARSET_HEX:
-      return `${HEX}`;
-    case CHARSET_HEX_LOWER:
-      return `${HEX_LOWER}`;
-    default:
-      const validCharSets = [
-        CHARSET_ALPHA,
-        CHARSET_ALPHA_LOWER,
-        CHARSET_DIGIT,
-        CHARSET_ALNUM,
-        CHARSET_BINARY,
-        CHARSET_OCTAL,
-        CHARSET_HEX,
-        CHARSET_HEX_LOWER
-      ];
-      throw new Error(`Invalid builtIn characterSet specified. Allowed values: ${validCharSets.join(', ')}`);
+  case CHARSET_ALPHA:
+    return ALPHABET_UPPERCASE;
+  case CHARSET_ALPHA_LOWER:
+    return ALPHABET_LOWERCASE;
+  case CHARSET_DIGIT:
+    return DIGIT;
+  case CHARSET_ALNUM:
+    return `${ALPHABET_UPPERCASE}${ALPHABET_LOWERCASE}${DIGIT}`;
+  case CHARSET_BINARY:
+    return `${BINARY}`;
+  case CHARSET_OCTAL:
+    return `${OCTAL}`;
+  case CHARSET_HEX:
+    return `${HEX}`;
+  case CHARSET_HEX_LOWER:
+    return `${HEX_LOWER}`;
+  default:
+    throw new Error(`Invalid builtIn characterSet specified. Allowed values: ${validCharSets.join(', ')}`);
   }
 }
 
