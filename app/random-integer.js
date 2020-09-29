8142ae1cc1d17cc5afadfc62dd519416848c1bd0
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-09-29 10:49:19 +0530

    update

--- app/random-integer.js
+++ app/random-integer.js
@@ -6,7 +6,6 @@
  * @param {number} max The ending integer value.
  * @returns {number} Random integer value between min and max both inclusive.
  */
-const randomInteger = (min, max) =>
-  Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
+const randomInteger = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 
 module.exports = randomInteger;
