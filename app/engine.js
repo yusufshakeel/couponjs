8c6567e2c325ee52ecd7ba0842d17da6b4ba2e3a
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-10-18 01:30:29 +0530

    refactor work

--- app/engine.js
+++ app/engine.js
@@ -15,6 +15,7 @@ const {
   MAX_NUMBER_OF_COUPONS_TO_GENERATE,
   DEFAULT_NUMBER_OF_COUPONS_TO_GENERATE,
   DEFAULT_OMIT_CHARACTERS,
+  DEFAULT_CHARACTER_SET_OPTION,
   UNDEFINED
 } = require('./constants.js');
 const Formatter = require('./formatter.js');
@@ -22,8 +23,8 @@ const characterSetBuilder = require('./character-set-builder.js');
 
 /**
  * Engine to produce coupon.
- * @param {object} characterSetOption This is the set of character set options used to generate coupon.
  * @param {function} randomInteger This is the function that will generate random integer value.
+ * @param {object} characterSetOption This is the set of character set options used to generate coupon.
  * @param {number} length This is the length of the coupon excluding prefix and suffix characters if any.
  * @param {string} prefix This is the set of characters that is added at the start of the coupon.
  * @param {string} suffix This is the set of characters that is added at the end of the coupon.
@@ -34,8 +35,8 @@ const characterSetBuilder = require('./character-set-builder.js');
  * @constructor
  */
 const Engine = function ({
-  characterSetOption,
   randomInteger,
+  characterSetOption = DEFAULT_CHARACTER_SET_OPTION,
   length = DEFAULT_LENGTH,
   prefix = DEFAULT_PREFIX,
   suffix = DEFAULT_SUFFIX,
