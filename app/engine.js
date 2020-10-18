c3c59d76bf137ef002cb3e0682de2094ba7e991e
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-10-18 10:29:50 +0530

    refactor

--- app/engine.js
+++ app/engine.js
@@ -25,7 +25,7 @@ const characterSetBuilder = require('./character-set-builder.js');
 /**
  * Engine to produce coupon.
  * @param {function} randomInteger This is the function that will generate random integer value.
- * @param {object} characterSetOption This is the set of character set options used to generate coupon.
+ * @param {{ builtIn: string[], custom: string[] }} characterSetOption This is the set of character set options used to generate coupon.
  * @param {number} length This is the length of the coupon excluding prefix and suffix characters if any.
  * @param {string} prefix This is the set of characters that is added at the start of the coupon.
  * @param {string} suffix This is the set of characters that is added at the end of the coupon.
