326b3b0a470de50fef2a523e1f93683cc42c712b
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2020-10-19 22:46:57 +0530

    docs

--- app/functional/index.js
+++ app/functional/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * This will sum up the values.
+ * @param {number[]} values
+ * @returns {number}
+ */
 const sumOf = values => values.reduce((sum, size) => sum + size, 0);
 
 module.exports = { sumOf };
